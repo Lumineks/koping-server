6173844cf5be3bd9bf996cc2ae24cb690b3de8ba
Michail <mihasik323@gmail.com>
2021-10-09 14:47:01 +0300

    Add ejs template engine

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,15 +1,16 @@
-import { Body, Controller, Get, Header, Post } from '@nestjs/common';
+import { Body, Controller, Get, Header, Post, Render } from '@nestjs/common';
 
 @Controller()
 class AppController {
   @Get()
+  @Render('index')
   index() {
-    return 'hello from nestjs';
+    return { message: 'Hello world!' };
   }
 
   @Get('/products')
   products(@Body() body: string) {
-    return {text: "hey there"};
+    return { text: 'hey there' };
   }
 }
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
+
   await app.listen(8080);
 }
 bootstrap();
